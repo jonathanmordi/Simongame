8849c1a9f9fa3149cdf47b19d92542e800a2884c
jonathanmordi <jmordi628@gmail.com>
2025-09-07 12:43:37 -0400

    inital commit

---
+++ Simon Game/game.js
@@ -0,0 +1,103 @@
+const gamePattern = [];
+const buttonColors = ["red", "blue", "green", "yellow"];
+let userClickedPattern = [];
+
+var level = 0;
+var started = false;
+
+$(document).keydown(function () {
+
+    if(!started){
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+    }
+   
+});
+
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    // console.log(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function () {
+            nextSequence();
+        },  1000);
+
+        }
+    }
+     else {
+        console.log("wrong");
+            var audio = new Audio('sounds/wrong.mp3');
+            audio.play()
+        $("body").addClass("game-over");
+            setTimeout(function() {
+            $("body").removeClass("game-over");
+            }, 200);
+        $("#level-title").text("Game over, Press any key to restart");
+        startOver();
+        
+     }
+}
+
+function nextSequence(){
+
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+
+    
+     $("#level-title").text("Level " + level)
+     level++;
+    
+     userClickedPattern = [];
+
+}
+
+function playSound(name){
+    var audio = new Audio('sounds/' + name + '.mp3');
+    audio.play()
+}
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed")
+   
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern.length = 0;
+    started = false;
+}
+
+$("#reset-button").on("click", function (){
+    startOver();
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+
+     $("#reset-button").addClass("pressed-reset")
+   
+    setTimeout(function() {
+        $("#reset-button").removeClass("pressed-reset");
+    }, 100);
+
+});
+
+
